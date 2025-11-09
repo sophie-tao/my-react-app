a8df23e312d86d02c6ee8293a0ca087c699e1bb1
Sophie Tao <sophie.t@wustl.edu>
2025-11-09 17:59:03 -0600

    Created and called handlePlay function

--- src/App.jsx
+++ src/App.jsx
@@ -56,10 +56,15 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    //todo
+  }
+
   return (
     <div className="game">
-      <div className="board">
-        <Board />
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>

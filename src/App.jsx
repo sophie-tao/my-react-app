568721264284349cdc040a56eb80e4efbbb1d3c6
Sophie Tao <sophie.t@wustl.edu>
2025-11-09 17:56:37 -0600

    Added state to Game component

--- src/App.jsx
+++ src/App.jsx
@@ -53,6 +53,8 @@ function Board() {
 }
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   return (
     <div className="game">
       <div className="board">

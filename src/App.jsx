9d78074a4de5e4290e0742a4e33d751fa66a6a75
Sophie Tao <sophie.t@wustl.edu>
2025-11-09 17:29:02 -0600

    Updated handleClick function to take index of the square

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function Square ({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick() {
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[0] = "X";
+    nextSquares[i] = "X";
     setSquares(nextSquares);
   }
 

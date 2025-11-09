01bb309acc8ac910eb1a50b3973c9b37d9949222
Sophie Tao <sophie.t@wustl.edu>
2025-11-09 17:40:31 -0600

    Fixed the mistake and moved the if statement to within handleClick

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ function Square ({ value, onSquareClick }) {
 }
 
 export default function Board() {
-  if (squares[i]) {
-    return;
-  }
-  
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
